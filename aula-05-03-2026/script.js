229843d7f8f0c776709bd59f4b18b24d9935cf71
HeitorReboucas <Heitoralvesreboucas@gmail.com>
2026-03-19 12:01:30 -0300

    Commit Aula(Estudar depois)

---
+++ aula-05-03-2026/script.js
@@ -0,0 +1,26 @@
+console.log("teste")
+
+/*Variaveis*/
+// permite reatribuição, escopo de bloco/global
+var variavel1 = 'Um';
+console.log(variavel1);
+
+//permite reatribuição, escopo de bloco
+let variavel2 = 'Dois';
+console.log(variavel2);
+
+//nn permite reatribuição, escopo global
+const variavel3 = 'Três';
+console.log(variavel3);
+var nome = window.prompt("Nome: ");
+console.log(nome)
+
+// algoritmo que realize a soma de 2 numeros
+var numero1 = window.prompt("Numero 1: ");
+var numero2 = window.prompt("Numero2: ")
+var somaNum = parseFloat(numero1) + parseFloat(numero2);
+console.log(`A some é ${somaNum}`)
+
+// Condicionais: if-else, switch-case
+
+// Estrutura de repetição:  
